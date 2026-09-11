3c1d697728e6aa80041ef5dd4782707bbf9a818b
bushkuhlreed1-debug <bushkuhlreed1@gmail.com>
2026-09-11 16:20:32 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,14 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Dirty P Fantasy Football",
+  description: "Dirty P Fantasy Football league history",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
